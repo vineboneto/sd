4d4b07ad8c53fa19e2917f61a52f5d95f671fedd
vineboneto <vineboneto@gmail.com>
2023-05-23 07:08:27 -0300

    feat: criado client para servidor alternativo

---
+++ bingo/client_marlon.py
@@ -0,0 +1,121 @@
+import grpc
+import time
+import threading
+import signal
+import numpy as np
+
+
+import proto.bingo_pb2 as bingo_pb2
+import proto.bingo_pb2_grpc as bingo_pb2_grpc
+
+URL = "localhost:8000"
+
+numbers_drafted = []
+
+
+def authenticate(username):
+    global auth
+
+    channel = grpc.insecure_channel(URL)
+
+    stub = bingo_pb2_grpc.BingoStub(channel)
+    responses = stub.Login(bingo_pb2.LoginRequest(username=username))
+
+    try:
+        for response in responses:
+            auth = {"username": username, "token": response.token}
+            print(f"\nToken: {response.token}")
+            print(f"Players loggedIn: {', '.join(list(response.playersLoggedIn))}")
+            print(f"Players Ready: {', '.join(list(response.playersReady))}")
+            print(f"message: {response.message}")
+            print(f"status: {response.status}")
+            print("\nwaiting for other players... press Ctrl+C to exit")
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed...")
+    finally:
+        channel.close()
+
+
+def ready():
+    global auth
+
+    with grpc.insecure_channel(URL) as channel:
+        stub = bingo_pb2_grpc.BingoStub(channel)
+        response = stub.Ready(bingo_pb2.ReadyRequest(username=auth.get("username"), token=auth.get("token")))
+    return response
+
+
+def play():
+    global auth
+
+    channel = grpc.insecure_channel(URL)
+
+    stub = bingo_pb2_grpc.BingoStub(channel)
+    responses = stub.Play(bingo_pb2.PlayRequest(username=auth.get("username"), token=auth.get("token")))
+
+    try:
+        for response in responses:
+            print(response)
+            numbers_drafted.append(response.number)
+            print(", ".join(map(str, numbers_drafted)))
+            print("\nwaiting for new number... press Ctrl+C to exit")
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed...")
+    finally:
+        channel.close()
+
+
+def check_win():
+    global auth
+
+    with grpc.insecure_channel(URL) as channel:
+        stub = bingo_pb2_grpc.BingoStub(channel)
+        response = stub.CheckWin(bingo_pb2.WinCheckRequest(username=auth.get("username"), token=auth.get("token")))
+    return response
+
+
+if __name__ == "__main__":
+    auth = {}
+
+    try:
+        execute = True
+        card = np.array([])
+        while execute:
+            if auth.get("username"):
+                print(f"\n{auth.get('username')} - {auth.get('token')}")
+            if card.any():
+                print(card)
+            print("1. Login")
+            print("2. Ready")
+            print("3. Play")
+            print("4. Check Win")
+            print(":q. Exit")
+            option = input("Enter option: ")
+            if option == "1":
+                username = input("Enter username: ")
+                authenticate(username)
+            elif option == "2":
+                if not auth.get("token"):
+                    print("You must login first")
+                else:
+                    response = ready()
+                    card = list(response.card)
+                    card.insert(12, -1)
+                    card = np.reshape(card, (5, 5))
+            elif option == "3":
+                if not auth.get("token"):
+                    print("You must login first")
+                else:
+                    play()
+            elif option == "4":
+                if not auth.get("token"):
+                    print("You must login first")
+                else:
+                    response = check_win()
+                    print(response)
+            elif option == ":q":
+                execute = False
+            else:
+                print("Invalid option")
+    except KeyboardInterrupt:
+        print("\nCtrl+C pressed...")
